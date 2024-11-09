9329d9907137f3c59a6699ffc189254996c61699
EberteSampaio <ebertechavessampaiojr@gmail.com>
2024-11-09 16:58:26 -0300

    init: first commit

---
+++ src/main/java/com/study_dio/eberte/EberteApplication.java
@@ -0,0 +1,13 @@
+package com.study_dio.eberte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EberteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EberteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study_dio/eberte/EberteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study_dio.eberte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EberteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
