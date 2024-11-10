aa7ff114ab5f9c51aa87257d8b359624051c402e
EberteSampaio <ebertechavessampaiojr@gmail.com>
2024-11-10 01:47:42 -0300

    feat: nwe implements

--- src/main/java/com/study_dio/eberte/EberteApplication.java
+++ src/main/java/com/study_dio/eberte/EberteApplication.java
@@ -1,12 +1,17 @@
 package com.study_dio.eberte;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server url")})
 @SpringBootApplication
 public class EberteApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EberteApplication.class, args);
 	}
 
